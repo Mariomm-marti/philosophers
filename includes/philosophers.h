f68a510e636f3dd62a52581d3abcda51e9d3acc8
Mario Martin moreno <mmartin-@student.42madrid.com>
2021-08-05 19:19:17 +0200

    Updated philosophers initialization

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 19:29:56 by mmartin-          #+#    #+#             */
+/*   Updated: 2021/08/05 18:42:24 by mmartin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
 # define MAX_TIMESTAMP 4294967295U
+
+# include <pthread.h>
+# include <timestamps.h>
 
 typedef enum e_bool
 {
@@ -9,27 +24,31 @@ typedef enum e_bool
 	true
 }	t_bool;
 
-enum e_philostatus
-{
-	THINKING,
-	EATING,
-	SLEEPING
-};
-
 typedef struct s_philo
 {
 	unsigned int		id;
-	enum e_philostatus	status;
-	struct s_philo		*next;
+	t_timestamp			initial_timestamp;
+	pthread_t			thread;
 }	t_philo;
+
+typedef struct s_fork
+{
+	pthread_mutex_t		mutex;
+}	t_fork;
 
 typedef struct s_params
 {
 	unsigned int	philos;
 	unsigned int	amount;
 	unsigned int	die;
-	unsigned int 	eat;
-	unsigned int 	sleep;
+	unsigned int	eat;
+	unsigned int	sleep;
 }	t_params;
+
+void		*worker(void *self);
+
+t_philo		*create_philos(unsigned int const philo_num);
+t_philo		*get_philo(t_philo const *philos, unsigned int const philo_id);
+void		free_philos(t_philo *master);
 
 #endif
--- includes/utils.h
+++ includes/utils.h
@@ -1,10 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 19:31:12 by mmartin-          #+#    #+#             */
+/*   Updated: 2021/08/05 18:06:17 by mmartin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef UTILS_H
 # define UTILS_H
 
 # include <stddef.h>
 
-size_t			ft_strlen(char const *str);
-void			ft_putstr(char const *str);
 unsigned long	ft_strtoul(char const *str);
 
 #endif
