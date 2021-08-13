c91286fa7aa876b498e8bc28e315cc1ad19ea24d
vim <vim@42urduliz.com>
2021-08-13 14:51:03 +0200

    Implemented forks

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 19:08:20 by mmartin-          #+#    #+#             */
-/*   Updated: 2021/08/12 20:11:53 by mmartin-         ###   ########.fr       */
+/*   Updated: 2021/08/13 14:45:20 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,28 @@ pthread_mutex_t	*init_mutex(size_t const mutex_num)
 	return (mutex);
 }
 
-static t_routine	init_routine(size_t const caller_id,
-		size_t const thread_num, pthread_mutex_t *mutex)
+t_routine	*init_routines(size_t const thread_num, pthread_mutex_t *mutex)
 {
-	t_routine	new_routine;
+	t_routine	*new_routines;
+	size_t		current_routine;
 
-	new_routine.caller_id = caller_id;
-	new_routine.thread_num = thread_num;
-	new_routine.mutex = mutex;
-	return (new_routine);
+	new_routines = malloc(sizeof(t_routine) * thread_num);
+	if (!new_routines)
+		return (NULL);
+	current_routine = 0;
+	while (current_routine < thread_num)
+	{
+		(new_routines + current_routine)->caller_id = current_routine;
+		(new_routines + current_routine)->thread_num = thread_num;
+		(new_routines + current_routine)->mutex = mutex;
+		current_routine++;
+	}
+	return (new_routines);
 }
 
-pthread_t	*init_threads(size_t const thread_num, pthread_mutex_t *mutex)
+pthread_t	*init_threads(size_t const thread_num, t_routine *routines)
 {
 	pthread_t	*threads;
-	t_routine	thread_routine;
 	size_t		current_thread;
 
 	threads = malloc(sizeof(pthread_t) * thread_num);
@@ -56,8 +63,8 @@ pthread_t	*init_threads(size_t const thread_num, pthread_mutex_t *mutex)
 	current_thread = 0;
 	while (current_thread < thread_num)
 	{
-		thread_routine = init_routine(current_thread, thread_num, mutex);
-		pthread_create(threads + current_thread, NULL, routine, &thread_routine);
+		pthread_create(threads + current_thread, NULL, routine,
+				routines + current_thread);
 		current_thread++;
 	}
 	return (threads);
@@ -66,10 +73,12 @@ pthread_t	*init_threads(size_t const thread_num, pthread_mutex_t *mutex)
 int		main(void)
 {
 	pthread_mutex_t	*mutex;
+	t_routine		*routines;
 	pthread_t		*threads;
 
 	mutex = init_mutex(10);
-	threads = init_threads(10, mutex);
+	routines = init_routines(10, mutex);
+	threads = init_threads(10, routines);
 	while (1)
 		;
 }
--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 18:40:22 by mmartin-          #+#    #+#             */
-/*   Updated: 2021/08/12 20:13:05 by mmartin-         ###   ########.fr       */
+/*   Updated: 2021/08/13 14:48:50 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,53 @@
 #include <unistd.h>
 
 static void	get_mutex_for_thread(size_t const caller_id,
-		size_t const thread_num)
+		size_t const thread_num, size_t *mutex_right, size_t *mutex_left)
 {
+	*mutex_right = caller_id;
+	if (caller_id == 0)
+		*mutex_left = thread_num - 1;
+	else
+		*mutex_left = caller_id - 1;
 }
 
-static void	lock_me(size_t const caller_id, size_t const thread_num,
+static void	lock_mutex(size_t const caller_id, size_t const thread_num,
 		pthread_mutex_t *mutex)
 {
-	size_t	mutex_one;
-	size_t	mutex_two;
+	size_t	mutex_right;
+	size_t	mutex_left;
 
+	get_mutex_for_thread(caller_id, thread_num, &mutex_right, &mutex_left);
+	pthread_mutex_lock(mutex + mutex_right);
+	printf("%zu has taken a fork (%zu | %p)\n", caller_id, mutex_right, mutex + mutex_right);
+	pthread_mutex_lock(mutex + mutex_left);
+	printf("%zu has taken a fork (%zu | %p)\n", caller_id, mutex_left, mutex + mutex_left);
+}
 
-	pthread_mutex_lock(mutex + caller_id);
+static void	unlock_mutex(size_t const caller_id, size_t const thread_num,
+		pthread_mutex_t *mutex)
+{
+	size_t mutex_right;
+	size_t mutex_left;
+
+	get_mutex_for_thread(caller_id, thread_num, &mutex_right, &mutex_left);
+	pthread_mutex_unlock(mutex + mutex_right);
+	pthread_mutex_unlock(mutex + mutex_left);
 }
 
 void	*routine(void *arg)
 {
 	t_routine const	*data = (t_routine *)arg;
 
+	if (data->caller_id % 2 != 0)
+		usleep(100);
 	while (1)
 	{
-		lock_me(data->caller_id, data->thread_num, data->mutex, pthread_mutex_lock, "%d has taken fork (%d)\n");
-		usleep(40000);
-		lock_me(data->caller_id, data->thread_num, data->mutex, pthread_mutex_unlock, "%d has left the fork (%d)\n");
+		lock_mutex(data->caller_id, data->thread_num, data->mutex);
+		printf("%zu is eating\n", data->caller_id);
+		sleep(2);
+		unlock_mutex(data->caller_id, data->thread_num, data->mutex);
+		printf("%zu is sleeping\n", data->caller_id);
+		sleep(1);
 	}
 	return (NULL);
 }
